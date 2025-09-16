9bf66d05775ce37b0dc253748c3e6b0254486111
BPillay27 <byronpillay2704@gmail.com>
2025-09-16 15:31:30 +0200

    Adjustments, still small issues to resolve.

--- Main.java
+++ Main.java
@@ -4,19 +4,70 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Main {
 
     enum Contention {
-        LOW(thinkMs(/*add here*/), work(/*add here*/)),
-        MEDIUM(thinkMs(/*add here*/), work(/*add here*/)),
-        HIGH(thinkMs(/*add here*/), work(/*add here*/));
+        LOW(thinkMs(150), work(2000)),
+        MEDIUM(thinkMs(50), work(8000)),
+        HIGH(thinkMs(0), work(25000));
 
         final int thinkMs;
         final int csWorkIters;
-        Contention(int thinkMs, int csWorkIters) { this.thinkMs = thinkMs; this.csWorkIters = csWorkIters; }
 
-        static int thinkMs(int v) { return v; }
-        static int work(int v) { return v; }
+        Contention(int thinkMs, int csWorkIters) {
+            this.thinkMs = thinkMs;
+            this.csWorkIters = csWorkIters;
+        }
+
+        static int thinkMs(int v) {
+            return v;
+        }
+
+        static int work(int v) {
+            return v;
+        }
+
+        int getThink() {
+            return thinkMs;
+        }
+
+        int getWork() {
+            return csWorkIters;
+        }
     }
 
     public static void main(String[] args) throws Exception {
-        //TODO: Test your Code
+        try {
+            Contention level = Contention.LOW;
+            int players = 2;
+            Lock _lock=new TTASLock();
+
+            if (args.length > 3) {
+                throw new IllegalArgumentException("Too many arguments. At most 2!");
+            }
+
+            if (args.length == 1) {
+                level = Contention.valueOf(args[0].toUpperCase());
+            } else if (args.length == 2) {
+                level = Contention.valueOf(args[0].toUpperCase());
+                players = Math.abs(Integer.parseInt(args[1]));
+            } else if (args.length==3){
+                level = Contention.valueOf(args[0].toUpperCase());
+                players = Math.abs(Integer.parseInt(args[1]));
+
+                if(args[2].equals("CLH")){
+                    _lock=new CLHLock();
+                }
+            }
+
+            System.out.println("RUNNING with " + args[0].toUpperCase() + " contention and " + args[1] + " players");
+            TreasureChest chest=new TreasureChest("Chest", 2000,_lock);
+            List<Player> playerList=new ArrayList<>();
+            for(int i;i<players;i++){
+                Player temp=new Player(String.valueOf(i),chest,level.thinkMs, level.csWorkIters);
+                playerList.add(new Player(String.valueOf(i),));
+            }
+
+        } catch (IllegalArgumentException e) {
+            System.err.println("Arguement exception: " + e);
+        }
+
     }
 }
